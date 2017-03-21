4ccf0d4a2652c9f6a6ec27b376800666a3065dac
Raphael <Blazylp@gmail.com>
2017-03-21 16:30:39 +0100

    added coinflip (a lot less code than before) also some improvements

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-      'Token' :'MjA2OTU1OTM1MjI5MjgwMjU2.CpZi3g.xWlcFQ7NI8Xq8uboMWq4rShh2F4',
+      'Token' :'MjA2OTU1OTM1MjI5MjgwMjU2.C7BBcA.09Z_-nB4gPFsMY_OjaWkoOviKbY',
       'prefix':'%'
 };
