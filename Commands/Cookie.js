957c17aad721f929b7c04690afd99efe313058bd
Raphael <Blazylp@gmail.com>
2017-04-23 08:19:58 +0200

    added some database stuff to Cookie

--- Commands/Cookie.js
+++ Commands/Cookie.js
@@ -1,6 +1,14 @@
+const fs = require("fs")
+let cookies = JSON.parse(fs.readFileSync('./databases/cookies.json', 'utf8'));
 exports.run = (client, msg) => {
     if (msg.mentions.users.first().size < 1) return msg.reply('You must mention someone for this Command.')
+    if (!cookies[msg.mentions.users.first().id]) cookies[msg.mentions.users.first().id] = {'cookies': 1};
+    cookies[msg.mentions.users.first().id].cookies++;
     msg.channel.sendMessage(`**${msg.mentions.users.first()} got a :cookie: from ${msg.author}**`)
+    fs.writeFile('./databases/cookies.json', JSON.stringify(cookies), err => {
+    if (err) console.error(err)
+    });
+    msg.channel.sendMessage(`${msg.mentions.users.first().username} has a total of ` + cookies[msg.mentions.users.first().id].cookies + ' Cookies!')
     console.log("[Command]     ", msg.author.username + "/" + msg.author.id, "(" + msg.content + ")")
 }
 
