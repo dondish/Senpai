f0b448273545b4a4a4b169640e9a670a103d6498
Raphael <Blazylp@gmail.com>
2017-05-04 17:13:21 +0200

    and finished updating code to D.js v11.1

--- Commands/disconnect.js
+++ Commands/disconnect.js
@@ -1,8 +1,8 @@
 exports.run = (client, msg) => {
-    if (msg.channel.type != "text") return msg.channel.sendMessage("You can run this command only on a Server!")
+    if (msg.channel.type != "text") return msg.channel.send("You can run this command only on a Server!")
     const voiceConnection = msg.guild.voiceConnection
     if (voiceConnection === null) return msg.reply("im not in a Voice Channel!")
-    msg.channel.sendMessage("By By :wave:")
+    msg.channel.send("By By :wave:")
     voiceConnection.disconnect()
     console.log("[Command]     ", msg.author.username + "/" + msg.author.id, "(" + msg.content + ")")
 }
--- Commands/help.js
+++ Commands/help.js
@@ -5,12 +5,12 @@ exports.run = (client, msg, params) => {
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     msg.channel.send("i've sent you an PM")
     .then(message => message.delete(10000))
-    msg.author.sendCode('asciidoc', `= Command List =\n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+    msg.author.send(`= Command List =\n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {"code": "asciidoc"});
   } else {
     let command = params[0].toUpperCase();
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      msg.channel.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nUsage: ${config.prefix}${command.help.usage}`);
+      msg.channel.send(`= ${command.help.name} = \n${command.help.description}\nUsage: ${config.prefix}${command.help.usage}`, {"code": "asciidoc"});
     }
   }
   console.log("[Command]     ", msg.author.username + "/" + msg.author.id, "(" + msg.content + ")")
