a1090672efbeb069bc583b4f45479d1689bb13d0
Raphael <Blazylp@gmail.com>
2017-05-08 15:51:44 +0200

    renamed clearsong to removesong!

--- Commands/removesong.js
+++ Commands/removesong.js
@@ -1,15 +1,15 @@
 const config                                = require('../config/config.js')
 exports.run = (client, msg, args) => {
     var play = client.commands.get("PLAY");
-    if(args.length > 1) return msg.reply("You must add the number of the Song in the queue")
+    if(args.length > 1) return msg.reply("You must add the number of the Song which you wanna remove!")
     var number = parseInt(args[0], 10)
-    if (number === 1) return msg.reply(`You try to delete the current playing song from the queue use ${config.prefix}skip!`)
+    if (number === 1) return msg.reply(`You try to delete the current playing song from the queue use ${config.prefix}skip instead`)
     play.deletesong(msg, number)
     console.log("[Command]     ", msg.author.username + "/" + msg.author.id, "(" + msg.content + ")")
 }
 
 exports.help = {
-    'name': 'clearsong',
+    'name': 'removesong',
     'description': 'delete 1 song from the queue',
-    'usage': 'clearsong <queue number>'
+    'usage': 'removesong <queue number>'
 }
