e7e40895bf6c0e03f0f54f3df03352fac1b908b4
Raphael <Blazylp@gmail.com>
2017-05-28 19:06:37 +0200

    updated ping (looks better now)

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,5 +1,16 @@
+const Discord = require('discord.js')
 exports.run = (client, msg) => {
-    msg.channel.send(Math.round(msg.client.ping) + " ms");
+        msg.channel.send('Pinging...').then(sent => {
+        const embed = new Discord.RichEmbed()
+        .setAuthor(`${msg.client.user.username}`, `${msg.client.user.avatarURL}`)
+        .setTitle("Pong! :ping_pong:")
+        .addField("Hearthbeat", `${Math.floor(msg.client.ping)}ms`  , true)
+        .addField("Message", `${sent.createdTimestamp - msg.createdTimestamp}ms`, true)
+        .setColor(0x80ff00)
+        .setTimestamp()
+        .setFooter("Senpai Bot by Yukine")
+        sent.edit({embed})
+        })
     console.log("[Command]     ", msg.author.username + "/" + msg.author.id, "(" + msg.content + ")")
 }
 
