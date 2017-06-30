fcd24479f01cb4a1e4bce52a999d0c49a3e52fbe
Raphael <Blazylp@gmail.com>
2017-06-30 14:53:26 +0200

    added uptime to about

--- Commands/Others/about.js
+++ Commands/Others/about.js
@@ -3,6 +3,17 @@ const Package = require('../../package.json')
 exports.run = (client, msg) => {
 const dateformatted = new Date().toISOString()
 
+                function format(seconds){
+                    function pad(s){
+                        return (s < 10 ? '0' : '') + s;
+                    }
+                var hours = Math.floor(seconds / (60*60));
+                var minutes = Math.floor(seconds % (60*60) / 60);
+                var seconds2 = Math.floor(seconds % 60);
+
+                return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds2);
+        }
+
     const embed = new Discord.RichEmbed()
         .setTitle("About Senpai version: " + Package.version)
         .setAuthor("Yukine", "https://images-ext-2.discordapp.net/.eJwFwVEOwiAMANC7cAA6KiuwxHiWUjqdUUcA_dDs7r73M-_2MIu5jVH7AiDlZcvWZW-Fa7WyP4E_PLh1cNHTCRFDTJ7mMOEMXLKQUEZN5B0FVZdJV1JGDaske6_XS9--enYYzfEHhdAiAA.COLvqNfJ1q2tD1aEnwOV-DNW7xQ?width=80&height=80")
@@ -13,6 +24,7 @@ const dateformatted = new Date().toISOString()
         .addField('Total Servers:', client.guilds.size, true)
         .addField('Total Users:', client.users.size, true)
         .addField('Total Shards:', `${client.shard.count}`, true)
+        .addField('Uptime', `${format(process.uptime())}`)
         .addField('GitHub', "https://github.com/Dev-Yukine/Senpai-Bot-Discord")
         .setTimestamp(dateformatted)
     msg.channel.send({embed})
