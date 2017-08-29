972d55edb6c6d3bee81f32f1e140eeaa9c126302
Raphael <blazylp@gmail.com>
2017-08-29 02:33:08 +0200

    fixed coinflip to now have all options

--- src/commands/economy/coinflip.js
+++ src/commands/economy/coinflip.js
@@ -14,15 +14,23 @@ class CoinflipCommand extends Commands {
 
     async run(msg, params) {
         try{
-            const gambleamount = Number(params[0])
-            let currency = this.client.guilds.get("199857240037916672").emojis.get("322135966322262056")
-            if(isNaN(gambleamount)) return msg.reply("that is not a valid number :thinking:")
             const data = await msg.member.getEconomy(this.client)
             if(!data) return msg.reply(`looks like you haven't registered for the economy system yet you can do that by using the register command!`)
+            let cash = data.cash
+            let change = params[0]
+            let gambleamount
+            if(change === "all" ||change === "-a"||change === "everything")
+                {
+                    gambleamount = cash
+                } else {
+                    gambleamount = params[0]
+                    gambleamount = Number(gambleamount)
+                }
+            let currency = this.client.guilds.get("199857240037916672").emojis.get("322135966322262056")
+            if(isNaN(gambleamount)) return msg.reply("that is not a valid number :thinking:")
             if(gambleamount > data.cash) return msg.reply("You dont have that much money")
             if(gambleamount <= 0) return msg.reply("Your amount must be more than 0!")
             const random = Math.random()
-            let cash = data.cash
             let message;
             if(random > 0.5) {
                 cash += gambleamount
