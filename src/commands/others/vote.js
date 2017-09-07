5c87a8f95e4d4e4c2a962e8320e6733a73d7ce07
Raphael <blazylp@gmail.com>
2017-09-08 00:04:43 +0200

    little fix to vote

--- src/commands/others/vote.js
+++ src/commands/others/vote.js
@@ -15,7 +15,7 @@ class VoteCommand extends Commands {
 
     async run(msg) {
         const embed = new RichEmbed()
-            .addField(`You can upvote Senpai on this Link [Click me!](${this.client.config.voteLink})`, "I appreciate that very much <3")
+            .addField(`You can upvote Senpai on this Link`, `[Click me!](${this.client.config.voteLink}) \n I appreciate that very much <3`)
             .setColor(0x80ff00)
             .setTimestamp()
             .setFooter("Senpai Bot by Yukine")
