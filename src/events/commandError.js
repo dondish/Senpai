18783c50076f9e7ef71c0abde6ca98211d9d18e8
Raphael <blazylp@gmail.com>
2017-10-02 02:27:43 +0200

    added travis and stuff to build and deleted uneeded logs in CommandError Event

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -8,10 +8,8 @@ class commandError extends Events {
 
 	run(error, messageEvent, msg) {
 		const { client } = this;
-		client.log.info('   ');
 		client.log.error(`[Error]   ${error.name}: ${error.message}`);
 		client.log.error(`          with this message ${msg.content}`);
-		client.log.info('   ');
 		client.users.get(client.config.ownerID).send(`The error: "${error.name}:${error.message}" has occured; Go to logs for more info.`);
 	}
 }
