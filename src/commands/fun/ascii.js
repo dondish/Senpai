caf1c41314dec2b4d5dd9da16f6826254a639649
Yukine <blazylp@gmail.com>
2017-10-23 14:04:03 +0200

    added new commands ascii and choose aswell as deleted uneeded git file

---
+++ src/commands/fun/ascii.js
@@ -0,0 +1,23 @@
+const Commands = require('../../structures/new/Command.js');
+const ascii = require('ascii-art');
+const info = {
+	name: 'ascii',
+	description: 'transform text to ascii art (only works on short sentence)',
+	aliases: [],
+	examples: ['ascii test', 'ascii this is cool']
+};
+
+class AsciiCommand extends Commands {
+	constructor(client, group) {
+		super(client, info, group);
+	}
+
+	run(msg, params) {
+		const text = params.join(' ');
+		ascii.font(text, 'Doom', output => {
+			msg.channel.send(output, { code: 'ascii' });
+		});
+	}
+}
+
+module.exports = AsciiCommand;
---
+++ src/commands/fun/choose.js
@@ -0,0 +1,26 @@
+const Commands = require('../../structures/new/Command.js');
+const { RichEmbed } = require('discord.js');
+const info = {
+	name: 'choose',
+	description: 'choose between different options ( , is the separator )',
+	aliases: [],
+	examples: ['choose i should go outside, i should continue coding, i should get a gf']
+};
+
+class ChooseCommand extends Commands {
+	constructor(client, group) {
+		super(client, info, group);
+	}
+
+	run(msg, params) {
+		const options = params.join(' ').split(',');
+		const result = options[Math.floor(Math.random() * options.length)];
+		const embed = new RichEmbed()
+			.addField('I choose the following:', result)
+			.setColor('RANDOM')
+			.setAuthor(msg.member.displayName, msg.author.displayAvatarURL);
+		msg.channel.send({ embed });
+	}
+}
+
+module.exports = ChooseCommand;
