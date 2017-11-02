852f5686b33b077cfffaa20c0cb7b1c060516d1f
Yukine <blazylp@gmail.com>
2017-11-02 01:05:24 +0100

    more destructering \o/

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,5 +1,5 @@
 const Events = require('../structures/new/Event.js');
-const snekfetch = require('snekfetch');
+const { post } = require('snekfetch');
 
 class LeaveEvent extends Events {
 	constructor(client) {
@@ -11,10 +11,10 @@ class LeaveEvent extends Events {
 		try {
 			const size = await guild.client.shard.fetchClientValues('guilds.size');
 			const guildsizes = size.reduce((prev, val) => prev + val, 0);
-			await snekfetch.post(`https://discordbots.org/api/bots/${guild.client.user.id}/stats`)
+			await post(`https://discordbots.org/api/bots/${guild.client.user.id}/stats`)
 				.set('Authorization', this.client.config.dBotsToken)
 				.send({ server_count: guildsizes }); // eslint-disable-line camelcase
-			await snekfetch.post(`https://bots.discord.pw/api/bots/${guild.client.user.id}/stats`)
+			await post(`https://bots.discord.pw/api/bots/${guild.client.user.id}/stats`)
 				.set('Authorization', this.client.config.discordBotsToken)
 				.send({ server_count: guildsizes }); // eslint-disable-line camelcase
 			this.client.log.info(`${guild.client.user.username} left the Guild ${guild.name} size is now ${guildsizes}`);
